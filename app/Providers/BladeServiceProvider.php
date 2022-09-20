b7f166019f8b1456bb2d2c8c4ec717a831209311
doanvankhai01 <khaidoan0110@gmail.com>
2022-09-20 17:21:28 +0700

    Ẩn hiện giao diện với quyền Auth , cập nhật

--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -29,10 +29,10 @@ class BladeServiceProvider extends ServiceProvider
             //hasrole là 1 cái tên
             //$expression là tên quyền được truyền tới
             if(Auth::user()){// user() là fix cứng, không thể thay đổi. Nếu người dùng có đăng nhập
-                // 1 quyền
-                if(Auth::user()->hasRoles($expression)){//kiểm tra người đăng nhập có quyền gì
-                    return true;
-                }
+                // // 1 quyền
+                // if(Auth::user()->hasRoles($expression)){//kiểm tra người đăng nhập có quyền gì
+                //     return true;
+                // }
                 // nhiều quyền
                 if(Auth::user()->hasAnyRoles($expression)){//kiểm tra người đăng nhập có những quyền gì
                     return true;
